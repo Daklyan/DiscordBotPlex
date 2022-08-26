4e82c72b89296c30fa22daa7b8c16a743742b57a
daklyan <daklyan@protonmail.com>
2022-08-26 17:56:58 +0200

    Add refresh login function

--- trakt_utils.py
+++ trakt_utils.py
@@ -4,8 +4,9 @@ import requests
 class Trakt:
     def __init__(self):
         self.TRAKT_API_URL = "https://api.trakt.tv"
-        self.REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
-
+        self.access_token = ""
+        self.refresh_token = ""
+        self.expire_date = 0
         with open('config.json') as f:
             data = json.load(f)
             self.trakt_client_id = data['trakt_client_id']
@@ -23,7 +24,7 @@ class Trakt:
         r = requests.post(url=f'{self.TRAKT_API_URL}/oauth/device/code', json={"client_id": self.trakt_client_id}, headers=self.HEADERS)
 
         data_device_code = r.json()
-    
+
         input(f'Enter {data_device_code["user_code"]} at https://trakt.tv/activate then press any key...')
 
         token_body = {
@@ -33,7 +34,28 @@ class Trakt:
         }
         r = requests.post(url=f'{self.TRAKT_API_URL}/oauth/device/token', json=token_body, headers=self.HEADERS)
         data_device_token = r.json()
-        print(r)
+        # TODO: Put access token, refresh token & expire date in a file
+        self.access_token = data_device_token['access_token']
+        self.refresh_token = data_device_token['refresh_token']
+        self.expire_date = data_device_token['created_at'] + data_device_token['expires_in']
+        return r.status_code
+
+    def refresh_login(self):
+        body = {
+            "refresh_token": self.refresh_token,
+            "client_id": self.trakt_client_id,
+            "client_secret": self.trakt_client_secret,
+            "redirect_uri": "urn:ietf:wg:oauth:2.0:oob",
+            "grant_type": "refresh_token"
+        }
+
+        r = requests.post(url=f'{self.TRAKT_API_URL}/oauth/token', json=body, headers=self.HEADERS)
+        data = r.json()
+        # TODO: Put access token, refresh token & expire date in a file
+        self.access_token = data['access_token']
+        self.refresh_token = data['refresh_token']
+        self.expire_date = data['created_at'] + data['expires_in']
+        return r.status_code
 
 
 def check_trakt_url(url):
