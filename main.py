1170b3df137823d49a78994a36cf4e7be50e44f8
daklyan <daklyan@protonmail.com>
2022-08-26 17:34:39 +0200

    Device login flow working

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import discord
 import json
+import trakt_utils
 
 
 class MyClient(discord.Client):
     async def on_message(self, message):
         if message.content.startswith("!add"):
+            pass
         # await message.channel.send("toast")
 
 
@@ -15,6 +17,9 @@ with open('config.json') as f:
 
 intents = discord.Intents.default()
 intents.message_content = True
+
+instance = trakt_utils.Trakt()
+instance.login()
 
 client = MyClient(intents=intents)
 client.run(token)
--- trakt_utils.py
+++ trakt_utils.py
@@ -1,24 +1,40 @@
 import json
 import requests
 
-from trakt import core
-from trakt import users
-from trakt import init
+class Trakt:
+    def __init__(self):
+        self.TRAKT_API_URL = "https://api.trakt.tv"
+        self.REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
 
+        with open('config.json') as f:
+            data = json.load(f)
+            self.trakt_client_id = data['trakt_client_id']
+            self.trakt_client_secret = data['trakt_client_secret']
+            self.lists = data['lists']
+            self.username = data['trakt_username']
 
-with open('config.json') as f:
-    data = json.load(f)
-    trakt_client_id = data['trakt_client_id']
-    trakt_client_secret = data['trakt_client_secret']
-    lists = data['lists']
-    username = data['trakt_username']
+        self.HEADERS = {
+            "Content-type": "application/json",
+            "trakt-api-key": self.trakt_client_id,
+            "trakt-api-version": "2"
+        }
 
+    def login(self):
+        r = requests.post(url=f'{self.TRAKT_API_URL}/oauth/device/code', json={"client_id": self.trakt_client_id}, headers=self.HEADERS)
 
+        data_device_code = r.json()
+    
+        input(f'Enter {data_device_code["user_code"]} at https://trakt.tv/activate then press any key...')
 
-core.AUTH_METHOD = core.OAUTH_AUTH
-core.OAUTH_TOKEN = init(username, client_id=trakt_client_id, client_secret=trakt_client_secret, store=True)
-my_user = users.User(username)
-print(my_user.lists)
+        token_body = {
+            "code": data_device_code['device_code'],
+            "client_id": self.trakt_client_id,
+            "client_secret": self.trakt_client_secret
+        }
+        r = requests.post(url=f'{self.TRAKT_API_URL}/oauth/device/token', json=token_body, headers=self.HEADERS)
+        data_device_token = r.json()
+        print(r)
+
 
 def check_trakt_url(url):
     request = requests.get(url=url)
